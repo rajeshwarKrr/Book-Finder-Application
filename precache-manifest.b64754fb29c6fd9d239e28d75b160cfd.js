b21d71667cb3da97a57673c4ab0d99d81c54f8cd
Rajeshwar Reddy <rajeshwarreddy.kadari@gmail.com>
2019-03-04 13:50:44 +0530

    Updates

---
+++ precache-manifest.b64754fb29c6fd9d239e28d75b160cfd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "00ae6fe4ed735dac1fcc",
+    "url": "/Book-Finder-Application/static/js/runtime~main.00ae6fe4.js"
+  },
+  {
+    "revision": "8bc7708daf95ea14ce58",
+    "url": "/Book-Finder-Application/static/js/main.8bc7708d.chunk.js"
+  },
+  {
+    "revision": "bb1cfbc77e0591448a8c",
+    "url": "/Book-Finder-Application/static/js/2.bb1cfbc7.chunk.js"
+  },
+  {
+    "revision": "fab756dff1f24f30bebe0ef21bc8f5f5",
+    "url": "/Book-Finder-Application/index.html"
+  }
+];
