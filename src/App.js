c6fb67e998140fc0e10c1c3fd7f507a34f9fe9c2
Rajeshwar Reddy <rajeshwarreddy.kadari@gmail.com>
2019-03-04 13:36:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import BooksList from "./BooksList";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      items: []
+    };
+    this.search = this.search.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  handleKeyPress(event) {
+    if (event.key === "Enter") this.search();
+  }
+
+  search() {
+    const query = this.state.query;
+    const key = "key=AIzaSyDzJSgN1WapYXNU84aSP_Z3vNv_xV-yXTA";
+    const BASE_URL = `https://www.googleapis.com/books/v1/volumes?q=${query}&${key}`;
+    console.log(BASE_URL);
+    fetch(BASE_URL, { method: "GET" })
+      .then(response => response.json())
+      .then(result => {
+        let books = result.items.map(item => {
+          return {
+            image:
+              item.volumeInfo.imageLinks !== undefined
+                ? item.volumeInfo.imageLinks.thumbnail
+                : "",
+
+            title: item.volumeInfo.title ? item.volumeInfo.title : "",
+            authors: item.volumeInfo.authors ? item.volumeInfo.authors : "",
+            publisher: item.volumeInfo.publisher
+              ? item.volumeInfo.publisher
+              : "",
+            link: item.volumeInfo.infoLink
+              ? item.volumeInfo.infoLink
+              : "https://books.google.com/"
+          };
+        });
+        this.setState({
+          items: books
+        });
+      });
+  }
+  handleChange(event) {
+    this.setState({
+      query: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Book Finder</h2>
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            this.search();
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Search for a book"
+            value={this.state.query}
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <div className="container">
+          <div className="row">
+            <BooksList propsItems={this.state.items} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/BooksList.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+export default class BooksList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.propsItems.map((item, i) => {
+          return (
+            <div className="col-4">
+              <div>
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="image-responsive"
+                />
+              </div>
+              <div className="">{item.title} </div>
+              <p>By: {item.authors}</p>
+              <p>Published By: {item.publisher}</p>
+              <a href={item.link} target="_blank">
+                See this Book
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
